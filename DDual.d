c5d87276c3cb62bfce499579c2967774aaa303cc
Tyler Olsen <tjolsen@mit.edu>
2016-05-21 09:32:56 -0400

    added more functions. tests work well. don't like that you have to qualify std.math.cos/sin/exp fully to use this.

--- DDual.d
+++ DDual.d
@@ -35,6 +35,10 @@ struct Dual(T) {
 	    return Dual!(T)(first*rhs.first, 
 			    second*rhs.first + first*rhs.second);
 	}
+        else static if (op == "/") {
+            return Dual!(T)(first/rhs.first,
+                            (second*rhs.first - first*rhs.second)/(rhs.first*rhs.first));
+        }
 	else { 
 	    static assert(0,"Operator '"~op~"' not implemented");
 	}
@@ -50,18 +54,40 @@ struct Dual(T) {
 }//end struct
 
 
+
+//------------------------------------------------------------
+// Utility function to make Dual!T instances
+//------------------------------------------------------------
 Dual!(T) make_dual(T)(T a, T b=0) {
     return Dual!(T)(a,b);
 }
-//Dual!(L) make_dual(T,L)(T a, L b=0) if(!isAssignable!(T,L)){
-//    return Dual!(L)(to!(L)(a),b);
-//}
+
+
+//------------------------------------------------------------
+// Overloads for analytical derivatives of transcendental functions
+//------------------------------------------------------------
+
+Dual!(T) sin(T)(Dual!(T) x) {
+    return Dual!(T)(std.math.sin(x.first), x.second*std.math.cos(x.first));
+}
+
+Dual!(T) cos(T)(Dual!(T) x) {
+    return Dual!(T)(std.math.cos(x.first), -x.second*std.math.sin(x.first));
+}
+
+Dual!(T) exp(T)(Dual!(T) x) {
+    T ex = std.math.exp(x.first);
+    return Dual!(T)(ex, x.second*ex);
+}
 
 
 
 unittest {
     import std.stdio;
 
+    //------------------------------------------------------------
+    // Test basic arithmetic operations
+    //------------------------------------------------------------
     auto a = make_dual(1.0,2.0);
     auto b = make_dual(1.0,2.0);
     assert(a == b);
@@ -82,8 +108,37 @@ unittest {
     auto h = a+2;
     assert(h == make_dual(3.0,2.0));
 
+    auto i = g/2;
+    assert(i == a);
+
+    //------------------------------------------------------------
+    // Test derivatives of easy functions
+    //------------------------------------------------------------
+    auto x = make_dual(2.0, 1.0);
+    double xp = (x*x).second; //test df/dx = 2x for f(x) = x*x
+    assert(xp == 2*x.first);
+
+    xp = (1/x).second;
+    assert(xp == -1/(x.first*x.first)); // test df/dx = -1/(x^2) for f(x) = 1/x
 
 
+    //------------------------------------------------------------
+    // Test derivatives of transcendental functions
+    //------------------------------------------------------------
+    auto cx = cos(x);
+    assert(abs(cx.first -std.math.cos(x.first)) < 1.0e-10);
+    assert(abs(cx.second - -x.second*std.math.sin(x.first))<1.0e-10);
+
+    auto sx = sin(x);
+    assert(abs(sx.first - std.math.sin(x.first)) < 1.0e-10);
+    assert(abs(sx.second - x.second*std.math.cos(x.first))<1.0e-10);
+
+    auto ex = exp(x);
+    assert(abs(ex.first - std.math.exp(x.first))<1.0e-10);
+    assert((ex.second - x.second*std.math.exp(x.first))<1.0e-10);
+
+
+    //------------------------------------------------------------
     writeln("All tests completed successfully");
 }
 version(unittest) {
